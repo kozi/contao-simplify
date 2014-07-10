37951d983cccb8af34ae400b58acf0b053f9a7d2
kozi <martin@kozianka.de>
2014-07-10 23:34:03 +0200

    Moved to composer

--- languages/de/tl_module.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['tl_module']['simplify_sorting']         = array('Sortierung', 'Sortierung der Beiträge');
-$GLOBALS['TL_LANG']['tl_module']['simplify_sorting_random']  = 'Zufällige Reihenfolge';
-$GLOBALS['TL_LANG']['tl_module']['simplify_sorting_asc']     = 'aufsteigend';
-$GLOBALS['TL_LANG']['tl_module']['simplify_sorting_desc']    = 'absteigend';
-
