ca7c0b1b07d323a1b84862bcfd54ce1e0a914101
Martin Kozianka <martin@kozianka.de>
2015-07-02 12:30:05 +0200

    Updated copyright infos

---
+++ contao/languages/de/tl_settings.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2015 Leo Feyer
+ *
+ *
+ * PHP version 5
+ * @copyright  Martin Kozianka 2012-2015 <http://kozianka.de/>
+ * @author     Martin Kozianka <http://kozianka.de/>
+ * @package    simplify
+ * @license    LGPL
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['tl_settings']['simplify_legend']                   = 'Simplify Einstellungen';
+$GLOBALS['TL_LANG']['tl_settings']['simplify_news']                     = ['Nachrichten-Modul vereinfachen', 'Aktiviert eine vereinfachte Ansicht des Nachrichtenmodul ohne Inhaltselemente.'];
+$GLOBALS['TL_LANG']['tl_settings']['simplify_calendar_events']          = ['Event-Modul vereinfachen', 'Aktiviert eine vereinfachte Ansicht des Eventmoduls ohne Inhaltselemente.'];
+$GLOBALS['TL_LANG']['tl_settings']['simplify_news_teaser']              = ['Nachrichten mit Teaser- und Nachrichtenfeld', 'Verwende ein Teaser- und ein Nachrichtenfeld im Nachrichten-Modul'];
